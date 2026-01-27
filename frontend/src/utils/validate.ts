bf662e792dea8957110b3d3c479e55f25a063202
jeonggyoungkim <jeonggyoungkim03@gmail.com>
2026-01-27 16:51:10 +0900

    refact: 로그인, 회원가입 타입 정의 분리

--- frontend/src/utils/validate.ts
+++ frontend/src/utils/validate.ts
@@ -1,22 +1,25 @@
-export type UserInformation = {
+export type LoginInformation = {
   email: string;
   password: string;
-  passwordConfirm?: string; 
-  nickname?: string;
+};
+
+export type SignupInformation = {
+  email: string;
+  password: string;
+  passwordConfirm: string;
+  nickname: string;
 };
 
 // 로그인 검증 함수
-function validateSigninUser(values: UserInformation) {
-  const errors = {
+function validateSignin(values: LoginInformation) {
+  const errors: Record<keyof LoginInformation, string> = {
     email: "",
     password: "",
   };
 
   if (!values.email) {
     errors.email = "이메일을 입력해주세요.";
-  } else if (
-    !/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i.test(values.email)
-  ) {
+  } else if (!/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i.test(values.email)) {
     errors.email = "올바른 이메일 형식이 아닙니다.";
   }
 
@@ -30,30 +33,24 @@ function validateSigninUser(values: UserInformation) {
 }
 
 // 회원가입 검증 함수
-function validateSignUpUser(values: UserInformation) {
-  const errors = {
+function validateSignup(values: SignupInformation) {
+  const errors: Record<keyof SignupInformation, string> = {
     email: "",
     password: "",
-    passwordConfirm: "", 
+    passwordConfirm: "",
     nickname: "",
   };
 
   if (!values.email) {
     errors.email = "이메일을 입력해주세요.";
-  } else if (
-    !/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i.test(
-      values.email
-    )
-  ) {
+  } else if (!/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i.test(values.email)) {
     errors.email = "올바른 이메일 형식이 아닙니다.";
   }
 
   if (!values.nickname) {
     errors.nickname = "닉네임을 입력해주세요.";
-  } else if (values.nickname.length < 2) {
-    errors.nickname = "닉네임은 2자 이상이어야 합니다.";
-  } else if (values.nickname.length > 10) {
-    errors.nickname = "닉네임은 10자 이하여야 합니다.";
+  } else if (values.nickname.length < 2 || values.nickname.length > 10) {
+    errors.nickname = "닉네임은 2자 이상 10자 이하여야 합니다.";
   }
 
   if (!values.password) {
@@ -64,7 +61,6 @@ function validateSignUpUser(values: UserInformation) {
     errors.password = "비밀번호는 영문자와 숫자를 포함해야 합니다.";
   }
 
-  // 비밀번호 재확인 
   if (!values.passwordConfirm) {
     errors.passwordConfirm = "비밀번호를 다시 입력해주세요.";
   } else if (values.password !== values.passwordConfirm) {
@@ -72,14 +68,6 @@ function validateSignUpUser(values: UserInformation) {
   }
 
   return errors;
-}
-
-function validateSignin(values: UserInformation) {
-  return validateSigninUser(values);
-}
-
-function validateSignup(values: UserInformation) {
-  return validateSignUpUser(values);
 }
 
 export { validateSignin, validateSignup };
