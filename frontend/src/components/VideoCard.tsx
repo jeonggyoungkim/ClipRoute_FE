3177550b0e04fd5c03f0c93e57eec3adaee0f568
jeonggyoungkim <jeonggyoungkim03@gmail.com>
2026-02-07 03:56:36 +0900

    style: 비디오 카드 레이아웃 및 썸네일 CSS 수정

--- frontend/src/components/VideoCard.tsx
+++ frontend/src/components/VideoCard.tsx
@@ -1,23 +1,24 @@
+import type { Course } from '../types/video';
+
 interface VideoCardProps {
-  title: string;
-  region: string;
-  duration: string;
-  thumbnailUrl?: string;
+  course: Course;
   onClick?: () => void;
 }
 
-const VideoCard = ({ title, region, duration, thumbnailUrl, onClick }: VideoCardProps) => {
+const VideoCard = ({ course, onClick }: VideoCardProps) => {
   return (
     <div 
       onClick={onClick}
-      className="bg-white rounded-3xl border border-gray-200 overflow-hidden cursor-pointer hover:shadow-md transition-shadow"
+      className="flex flex-col items-start w-[22.0625rem]
+    px-[0.9375rem] py-[0.75rem]
+    gap-[0.625rem]bg-white rounded-2xl border border-[#E4E4E4] cursor-pointer "
     >
-      {/*썸네일*/}
-      <div className="w-full aspect-video bg-gray-100 flex items-center justify-center m-3 rounded-2xl overflow-hidden">
-        {thumbnailUrl ? (
+      {/* 썸네일 */}
+      <div className="h-[10rem] self-stretch rounded-xl overflow-hidden">
+        {course.thumbnailUrl ? (
           <img 
-            src={thumbnailUrl} 
-            alt={title}
+            src={course.thumbnailUrl} 
+            alt={course.videoTitle}
             className="w-full h-full object-cover"
           />
         ) : (
@@ -27,16 +28,20 @@ const VideoCard = ({ title, region, duration, thumbnailUrl, onClick }: VideoCard
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M21 15l-5-5L5 21"/>
           </svg>
         )}
+        
       </div>
 
-      {/*영상 정보*/}
-      <div className="px-5 pb-4">
-        <h3 className="text-sm font-semibold text-gray-900 mb-3 whitespace-pre-line">
-          {title}
+      {/* 영상 정보 */}
+      <div className="px-1 pb-1">
+        <h3 className="text-sm font-semibold text-black mt-2">{course.channelName}</h3>
+        <h3 className="text-sm font-semibold text-gray-900 mb-2 whitespace-pre-line">
+          {course.videoTitle}
         </h3>
+        
+
         <div className="flex gap-2">
-          <span className="text-sm text-[#42BCEB]">#{region}</span>
-          <span className="text-sm text-[#42BCEB]">#{duration}</span>
+          <span className="text-sm text-[#42BCEB] font-semibold">#{course.regionName}</span>
+          <span className="text-sm text-[#42BCEB] font-semibold">#{course.travelDays}박 {course.travelDays + 1}일</span>
         </div>
       </div>
     </div>
