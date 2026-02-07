d0c34d1d37f297b51d0a0fff7bb1567dc0a172fb
jeonggyoungkim <jeonggyoungkim03@gmail.com>
2026-02-07 18:30:19 +0900

    feat: Mock API 사용 여부 설정을 위한 axios 인스턴스 구성

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
+
+const USE_MOCK = true; // Mock 사용: true, 실제 API: false
+
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
-  //baseURL: 'http://15.164.94.220:8080', 
+  baseURL: USE_MOCK ? '' : 'http://localhost:8080',
+  // baseURL: 'http://15.164.94.220:8080', // 배포 서버
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export default api;
+export { USE_MOCK };
